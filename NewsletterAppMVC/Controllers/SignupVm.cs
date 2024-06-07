6f837810ce6a12649aae0359322d6cf6c1a6c899
Alan Perlin <kuneck@gmail.com>
2024-06-07 20:48:58 +0100

    Add project files.

---
+++ NewsletterAppMVC/Controllers/SignupVm.cs
@@ -0,0 +1,9 @@
+﻿namespace NewsletterAppMVC.Controllers
+{
+    internal class SignupVm
+    {
+        public string FirstName { get; internal set; }
+        public string EmailAddress { get; internal set; }
+        public string LastName { get; internal set; }
+    }
+}
