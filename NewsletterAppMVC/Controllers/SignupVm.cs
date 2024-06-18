68918a1b7380535e509d0e3497351c415c94ab69
Alan Perlin <kuneck@gmail.com>
2024-06-18 10:12:42 +0100

    Update

--- NewsletterAppMVC/Controllers/SignupVm.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace NewsletterAppMVC.Controllers
-{
-    internal class SignupVm
-    {
-        public string FirstName { get; internal set; }
-        public string EmailAddress { get; internal set; }
-        public string LastName { get; internal set; }
-    }
-}
